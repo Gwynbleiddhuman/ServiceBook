3ce2edd4cce758201b33d3cb456d02ace0254257
Nikita <slynkons@gmail.com>
2026-03-31 07:47:23 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'carbook-nuke-v1';
+const CACHE_NAME = 'carbook-nuke-v2';
 
 self.addEventListener('install', event => {
   self.skipWaiting(); // Принудительно убиваем старый Service Worker
